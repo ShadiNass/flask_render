45fa596e4e0469e4b4d62c2de7ea8e96fe3190cc
ShadiNass <shadinass1@gmail.com>
2022-11-10 14:19:12 +0200

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
 import json
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 arr = [{"name":"me", "age":45},{"name":"aaaa", "age":15},{"name":"bbbbb", "age":50}]
 
