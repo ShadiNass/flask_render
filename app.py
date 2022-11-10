b0890774aa8eff8e9f50d3e687278ad2006e9bde
ShadiNass <shadinass1@gmail.com>
2022-11-10 14:03:32 +0200

    3rd

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 
-arr = [{"name":"me", "age":45}]
+arr = [{"name":"me", "age":45},{"name":"aaaa", "age":15},{"name":"bbbbb", "age":50}]
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return json.dumps(arr)
 
 
 if __name__ == '__main__':
